4f5b5accc44119d7698231a05b0191c20e48aab7
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2024-05-30 22:28:13 -0500

    minor fixes

--- routes/rent/rent.js
+++ routes/rent/rent.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const verifyAdministratorOrEditor = require("../../middleware/verifyAdministratorOrEditor");
 const { createRent } = require("../../controllers/rent/rentController");
-router.route("/").post(createRent);
+router.route("/").post(verifyAdministratorOrEditor, createRent);
 module.exports = router;
